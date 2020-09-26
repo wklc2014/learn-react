28a58d59e808ae4d5669c5937ae9cafdeb9a622f
wangkun <373658358@qq.com>
2020-09-26 19:00:00 +0800

    add:watermark

---
+++ src/components/water-mark/example/demo.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+class Demo extends Component {
+
+  static defaultProps = {
+
+  }
+
+  render() {
+    const {
+
+    } = this.props;
+
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
+}
+
+Demo.propTypes = {
+
+}
+
+export default Demo;
